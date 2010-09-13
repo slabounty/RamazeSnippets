675b04f5abcc38246706685817fd17807c7b2687
slabounty <slabounty@gmail.com>
2010-09-12 19:01:24 -0700

    Fixed model/init.rb so that it could connect to a Heroku database

--- model/init.rb
+++ model/init.rb
@@ -1,10 +1,9 @@
 require 'rubygems'
 require 'sequel'
 
-# Open the employee database. This must be done before we access the models
+# Open the library's database. This must be done before we access the models
 # that use it.
-DB = Sequel.sqlite("snippets.sqlite")
-
+Sequel.connect(ENV['DATABASE_URL'] || 'sqlite://snippets.sqlite')
 
 # Here go your requires for models:
 require 'model/models'
